21a13ac28952a88f6a39f94f4f4cf29872949e60
Ambreen Khan <annee_1210@hotmail.com>
2024-06-28 18:59:19 +1000

    Solutions added for returning the number of coin combinations and the fewest number of coins that make up a given amount.

--- src/main/java/leetCode/dynamic/CoinsChange.java
+++ src/main/java/leetCode/dynamic/CoinsChange.java
@@ -1,5 +1,7 @@
 package leetCode.dynamic;
 
+import java.util.Arrays;
+
 public class CoinsChange {
     public int change(int amount, int[] coins) {
         int[] allAmounts = new int[amount+1];
@@ -14,6 +16,24 @@ public class CoinsChange {
         }
 
         return allAmounts[allAmounts.length-1];
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        int[] allAmounts = new int[amount+1];
+        Arrays.fill(allAmounts, Integer.MAX_VALUE);
+        allAmounts[0] = 0;
+
+        for (int coin : coins) {
+            for (int j = 1; j <= amount; j++) {
+                if (coin <= j && allAmounts[j - coin]!= Integer.MAX_VALUE) {
+                    allAmounts[j] = Math.min(allAmounts[j],  1+ allAmounts[j - coin]);
+                }
+            }
+        }
+
+        int minAmounts = allAmounts[allAmounts.length-1];;
+        return (minAmounts == Integer.MAX_VALUE) ? -1 : minAmounts;
+
     }
 
 }
